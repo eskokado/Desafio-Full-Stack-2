4a1d0631bb236f99518a0c0d973d7482cda48637
eskokado <eskokado@gmail.com>
2023-07-27 17:04:45 -0300

    feat: config migration

---
+++ knexfile.js
@@ -0,0 +1,18 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  client: 'mysql',
+  connection: {
+    host: '127.0.0.1',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'db_next_tests'
+  },
+  migrations: {
+    directory: './migrations',
+  },
+};
