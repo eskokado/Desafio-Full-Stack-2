7f24e3bd57f5d87862092f8c225d0e1c86087449
eskokado <eskokado@gmail.com>
2023-07-28 15:12:08 -0300

    feat: create HomePage

--- frontend/src/pages/HomePage/index.jsx
+++ frontend/src/pages/HomePage/index.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { StyledHomePage } from "./styles";
+import { Container } from "../../styles/container";
+import { UserContext } from '../../contexts/UserContext'
+import { Navbar } from '../../components/Navbar'
 
 export const HomePage = () => {
+  const { onLogout, loading } = useContext(UserContext);
+
+  if (loading) {
+    return null;
+  }
+
   return (
-    <div>HomePage</div>
+    <StyledHomePage>
+      <section>
+        <Container>
+          <Navbar labelButton="Sair" onClick={onLogout} />
+        </Container>
+      </section>
+      <section>
+        <Container>
+        </Container>
+      </section>
+    </StyledHomePage>
   )
 }
