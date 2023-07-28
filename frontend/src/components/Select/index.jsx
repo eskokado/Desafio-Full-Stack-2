c3972a17c78aa5fc8b7492703c804fbb3ed98689
eskokado <eskokado@gmail.com>
2023-07-28 15:07:17 -0300

    feat: create component select

---
+++ frontend/src/components/Select/index.jsx
@@ -0,0 +1,30 @@
+import { StyledSelect } from "./styles";
+
+export const Select = ({
+  placeholder,
+  register,
+  field,
+  defaultValues,
+  disabled,
+  options,
+}) => {
+  return (
+    <StyledSelect
+      placeholder={placeholder}
+      {...register(field)}
+      disabled={disabled}
+    >
+      {options.map((option) =>
+        option.value === defaultValues[field] ? (
+          <option key={option.value} value={option.value} selected>
+            {option.text}
+          </option>
+        ) : (
+          <option key={option.value} value={option.value}>
+            {option.text}
+          </option>
+        )
+      )}
+    </StyledSelect>
+  );
+};
