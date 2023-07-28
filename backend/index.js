4c9d28bdc78bae61ea714556abfdfd3a6e6aec59
eskokado <eskokado@gmail.com>
2023-07-28 15:05:54 -0300

    feat: config cors into server

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const express = require('express')
+const cors = require('cors')
+
 const app = express();
 const port = 5000;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use(cors())
 
 app.get('/', async (req, res) => {
   res.send({
