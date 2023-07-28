bb6b087a320a865f7308463474f821d7c13c8409
eskokado <eskokado@gmail.com>
2023-07-28 03:26:01 -0300

    fix: add status code 404 user not found and optional status

--- backend/controllers/TransactionController.js
+++ backend/controllers/TransactionController.js
@@ -4,6 +4,12 @@ module.exports = {
   async index(req, res) {
     try {
       const user_id = req.user_id
+      const existsUser = await db('users').select('*').where('id', user_id).first()
+      if (!existsUser) {
+        return res.status(404).send({
+          message: 'User not found',
+        })
+      }
       const transactions = await db('transactions').select('*').where('user_id', user_id)
       return res.status(200).send({
         transactions
@@ -19,13 +25,19 @@ module.exports = {
     try {
       const user_id = req.user_id
       const {cpf, description, point, value, status} = req.body
-      if (!cpf || !description || !point || !value || !status) {
+      if (!cpf || !description || !point || !value) {
         return res.status(400).send({
           message: 'Please fill the fields',
         })
       } else {
+        const existsUser = await db('users').select('*').where('id', user_id).first()
+        if (!existsUser) {
+          return res.status(404).send({
+            message: 'User not found',
+          })
+        }
         const result = await db('transactions').insert({
-          cpf, description, point, value, status, user_id
+          cpf, description, point, value, status: status ?? 'Em avaliação', user_id
         })
         const data = await db('transactions').select('*').where('id', result[0]).first()
         if (result) {
