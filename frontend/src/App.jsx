f0f573e24286eb1998db28da98bfc365fd20b375
eskokado <eskokado@gmail.com>
2023-07-28 15:09:57 -0300

    feat: impl UserContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { ToastContainer } from "react-toastify";
 import { RoutesMain } from "./routes";
 import Global from "./styles/global";
+import { UserProvider } from './contexts/UserContext'
 
 function App() {
   return (
     <>
       <Global />
-      <RoutesMain />
+      <UserProvider>
+        <RoutesMain />
+      </UserProvider>
       <ToastContainer position="top-center" />
     </>
   );
