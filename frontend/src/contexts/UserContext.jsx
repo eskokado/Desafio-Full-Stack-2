e6d0add2229c55947544920df0277958ac263364
eskokado <eskokado@gmail.com>
2023-07-28 15:09:30 -0300

    feat: create UserContext with onLogin, onLogout, onRegister, autoLogin and user

--- frontend/src/contexts/UserContext.jsx
+++ frontend/src/contexts/UserContext.jsx
@@ -1,11 +1,100 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+import { api } from "../services/api";
 
 export const UserContext = createContext({});
 
 export const UserProvider = ({ children }) => {
+
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    autoLogin();
+  }, []);
+
+  const autoLogin = async () => {
+    setLoading(true);
+    const token = localStorage.getItem("@NEX_TOKEN");
+    if (!token) {
+      setLoading(false);
+      return;
+    }
+    try {
+      const { data } = await api.get("users/me", {
+        headers: {
+          user_access_token: `${token}`,
+        },
+      });
+      setUser(data);
+      navigate("/home");
+    } catch (error) {
+      localStorage.removeItem("@NEX_TOKEN");
+      localStorage.removeItem("@NEX_USERID");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onLogout = () => {
+    localStorage.removeItem("@NEX_TOKEN");
+    localStorage.removeItem("@NEX_USERID");
+    setUser(null);
+    navigate("/login");
+  };
+
+  const onLogin = async (data) => {
+    try {
+      setLoading(true);
+      const response = await toast.promise(api.post("auth/login", data), {
+        pending: "Efetuando login pendente...",
+        success: "Login efetuado com sucesso",
+      });
+      localStorage.setItem("@NEX_TOKEN", response.data.token);
+      localStorage.setItem("@NEX_USERID", response.data.user.id);
+      setUser(response.data.user);
+      const loggedSuccess = () => navigate("/home");
+      loggedSuccess();
+    } catch (error) {
+      const notify = () => toast.error("Falha ao logar!!!");
+      notify();
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onRegister = async (data) => {
+    try {
+      setLoading(true);
+      await toast.promise(api.post("users/create", data), {
+        pending: "Cadastrando novo usuário...",
+        success: "Novo usuário cadastrado com sucesso",
+      });
+      const registeredSuccess = () => navigate("/login");
+      registeredSuccess();
+    } catch (error) {
+      const notify = () => toast.error("Não foi possível cadastrar o usuário");
+      notify();
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <UserContext.Provider>
+    <UserContext.Provider    
+      value={{
+        loading,
+        user,
+        autoLogin,
+        onLogout,
+        onLogin,
+        onRegister,
+      }}
+    >
       {children}
     </UserContext.Provider>
   );
