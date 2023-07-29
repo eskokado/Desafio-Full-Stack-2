6e0114d5c13adc7707f84e8674c1c3a5403e70df
eskokado <eskokado@gmail.com>
2023-07-29 08:47:53 -0300

    fix: remove fields and add news fields into FormFilterTransactionModal

--- frontend/src/pages/HomePage/components/FormFilterTransactionModal/FormFilterTransactionModalContent/index.jsx
+++ frontend/src/pages/HomePage/components/FormFilterTransactionModal/FormFilterTransactionModalContent/index.jsx
@@ -11,25 +11,29 @@ import { TransactionContext } from "../../../../../contexts/TransactionContext";
 export const FormFilterTransactionModalContent = () => {
   const {
     loading,
-    onCreateTransaction,
-    setShowTransactionModal,
-    transaction,
-    onUpdateTransaction,
-    onRemoveTransaction,
+    onListTransaction,
+    setShowFilterTransactionModal,
   } = useContext(TransactionContext);
 
-  const formSchema = transaction
-    ? yup.object().shape({
-        status: yup.string().required("Nível é obrigatório"),
-      })
-    : yup.object().shape({
-        description: yup.string().required("Título é obrigatório"),
-        status: yup.string().required("Nível é obrigatório"),
-      });
+  const formSchema = yup.object().shape({
+    cpf: yup.string(),
+    description: yup.string(),
+    dateFrom: yup.string(),
+    dateTo: yup.string(),
+    valueFrom: yup.string(),
+    valueTo: yup.string(),
+    status: yup.string(),
+  });
+
 
   const defaultValues = {
-    description: transaction?.description ?? "",
-    status: transaction?.status ?? "",
+    cpf: "",
+    description: "",
+    dateFrom: "",
+    dateTo: "",
+    valueFrom: "",
+    valueTo: "",
+    status: "",
   };
 
   const {
@@ -42,18 +46,8 @@ export const FormFilterTransactionModalContent = () => {
   });
 
   const onSubmitFunction = (data) => {
-    if (transaction) {
-      delete data.description;
-      onUpdateTransaction(data);
-    } else {
-      onCreateTransaction(data);
-    }
-    setShowTransactionModal(false);
-  };
-
-  const handleRemove = (id) => {
-    onRemoveTransaction(id);
-    setShowTransactionModal(false);
+    onListTransaction(data);
+    setShowFilterTransactionModal(false);
   };
 
   const optionsStatus = [
@@ -66,11 +60,50 @@ export const FormFilterTransactionModalContent = () => {
   return (
     <StyledFormFilterTransactionModalContent onSubmit={handleSubmit(onSubmitFunction)}>
       <GroupInput
+        label="CPF"
+        placeholder="Digite aqui o CPF"
+        helperMessage={errors.cpf?.message && errors.cpf.message}
+        field="cpf"
+        defaultValues={defaultValues}
+        register={register}
+      />
+      <GroupInput
         label="Descrição"
         placeholder="Digite aqui a descrição"
-        helperMessage={errors.title?.message && errors.title.message}
+        helperMessage={errors.description?.message && errors.description.message}
         field="description"
-        disabled={transaction ? true : false}
+        defaultValues={defaultValues}
+        register={register}
+      />
+      <GroupInput
+        label="Data a partir de"
+        placeholder="Digite aqui a data inicial"
+        helperMessage={errors.dateFrom?.message && errors.dateFrom.message}
+        field="dateFrom"
+        defaultValues={defaultValues}
+        register={register}
+      />
+      <GroupInput
+        label="Data até"
+        placeholder="Digite aqui a data final"
+        helperMessage={errors.dateTo?.message && errors.dateTo.message}
+        field="dateTo"
+        defaultValues={defaultValues}
+        register={register}
+      />
+      <GroupInput
+        label="Valor a partir de"
+        placeholder="Digite aqui o valor inicial"
+        helperMessage={errors.valueFrom?.message && errors.valueFrom.message}
+        field="valueFrom"
+        defaultValues={defaultValues}
+        register={register}
+      />
+      <GroupInput
+        label="Valor até"
+        placeholder="Digite aqui o valor final"
+        helperMessage={errors.valueTo?.message && errors.valueTo.message}
+        field="valueTo"
         defaultValues={defaultValues}
         register={register}
       />
@@ -84,37 +117,15 @@ export const FormFilterTransactionModalContent = () => {
         disabled={false}
         options={optionsStatus}
       ></GroupSelect>
-      {transaction ? (
-        <div>
-          <Button
-            type="submit"
-            buttoncolor="primary50"
-            buttonstyle="default"
-            disabled={loading}
-          >
-            {loading ? "Alterando..." : "Salvar alterações"}
-          </Button>
-          <Button
-            type="button"
-            buttoncolor="grey1"
-            buttonstyle="default"
-            disabled={loading}
-            onClick={() => handleRemove(transaction.id)}
-          >
-            {loading ? "Excluindo..." : "Excluir"}
-          </Button>
-        </div>
-      ) : (
-        <Button
-          type="submit"
-          buttoncolor="primary50"
-          buttonwidth="max"
-          buttonstyle="default"
-          disabled={loading}
-        >
-          {loading ? "Cadastrando..." : "Cadastrar Transação"}
-        </Button>
-      )}
+      <Button
+        type="submit"
+        buttoncolor="primary50"
+        buttonwidth="max"
+        buttonstyle="default"
+        disabled={loading}
+      >
+        {loading ? "Pesquisando..." : "Pesquisar Transações"}
+      </Button>
     </StyledFormFilterTransactionModalContent>
   );
 };
