b587f3dde951e2f8554b27f181bc0b3957c455a5
eskokado <eskokado@gmail.com>
2023-07-29 08:51:19 -0300

    fix: add useEffect of onListTransaction into HomePage

--- frontend/src/pages/HomePage/index.jsx
+++ frontend/src/pages/HomePage/index.jsx
@@ -1,17 +1,27 @@
-import React, { useContext } from 'react'
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useContext, useEffect } from 'react'
 import { StyledHomePage } from "./styles";
 import { Container } from "../../styles/container";
 import { UserContext } from '../../contexts/UserContext'
 import { Navbar } from '../../components/Navbar'
 import { Header } from '../../components/Header'
 import { MainHome } from './components/MainHome'
+import { TransactionContext } from '../../contexts/TransactionContext'
 
 export const HomePage = () => {
   const { onLogout, loading } = useContext(UserContext);
+  const { onListTransaction } = useContext(TransactionContext);
 
   if (loading) {
     return null;
   }
+
+  useEffect(() => {
+    const listTransaction = async () => {
+      await onListTransaction()
+    }
+    listTransaction();
+  }, []);
 
   return (
     <StyledHomePage>
