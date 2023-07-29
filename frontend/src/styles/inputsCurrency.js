7cc88060e5c6454b06c2d6176e0669638e168669
eskokado <eskokado@gmail.com>
2023-07-29 10:13:49 -0300

    feat: create styled components by input currency

---
+++ frontend/src/styles/inputsCurrency.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import InputCurrency from 'react-currency-input-field';
+
+export const StyledInputCurrency = styled(InputCurrency)`
+  padding: 0 1.6rem;
+  font-size: 1.625rem;
+  border-radius: 0.4rem;
+  border-color: 0.122rem solid var(--color-grey-2);
+  background-color: var(--color-grey-2);
+  color: var(--color-text);
+  height: 4.8rem;
+  &:hover {
+    filter: brightness(1.2);
+  }
+`;
